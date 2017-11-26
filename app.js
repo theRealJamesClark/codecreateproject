ee55355ec1bb64d0b5cd613bbbf29aa8859ed89c
theRealJamesClark <33969606+theRealJamesClark@users.noreply.github.com>
2017-11-26 15:34:51 +1100

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+var size = 0;
+// 1. Receive posts
 function postMessage() {
 	console.log(document.getElementById("postedMessage").value);
+	if (size < 8) {
+		createMessage();
+	}
+	
 }
+
+// 2. Create new post with previous value
+function createMessage () {
+	var para = document.createElement("p");
+	var node = document.createTextNode(document.getElementById("postedMessage").value);
+	para.appendChild(node);
+
+	var element = document.getElementById("divNewMessages");
+	element.appendChild(para);
+	size++;
+}
+
+
+
+
