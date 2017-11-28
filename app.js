8d14318452781953e8e81ed0973e8736e816a221
theRealJamesClark <33969606+theRealJamesClark@users.noreply.github.com>
2017-11-28 11:50:20 +1100

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
-var size = 0;
-// 1. Receive posts
-function postMessage() {
-	console.log(document.getElementById("postedMessage").value);
-	if (size < 8) {
-		createMessage();
-	}
-	document.getElementById("form").reset();
-	
+
+var fireHeading = document.getElementById("fireHeading");
+var firebaseHeadingRef = firebase.database().ref().child("Heading");
+firebaseHeadingRef.on('value', function(data) {
+	fireHeading.innerText = data.val();
 }
 
+// Page Loadup
+function pageLoadUp() {
+	// ??
+	let reference = 0;
+	while (reference < size) {
+		loadItem(reference);
+		reference++;
+	}
+}
+
+function loadItem(reference) {
+}
+
+// New Message Functions
+var size = 0;
+var newMessage;
+// 1. Receive posts
+function postMessage() {
+	newMessage = document.getElementById("postedMessage").value;
+	console.log(newMessage);
+	//createMessage();
+	storeMessage();
+	
+	document.getElementById("form").reset();
+}
+
+// 2. Store in database
+function storeMessage() {
+	var firebaseRef = firebase.database().ref();
+	firebaseRef.push().set(newMessage);
+}
+/*
 // 2. Create new post with previous value
 function createMessage () {
+
 	var listItem = document.createElement("li");
 	var node = document.createTextNode(document.getElementById("postedMessage").value);
 	listItem.appendChild(node);
@@ -20,9 +49,10 @@ function createMessage () {
 	
 	listItem.classList.add('Messages');
 	
+	
 	size++;
+	
+
 }
 
-
-// 3. Like/Echo Function
-
+*/
